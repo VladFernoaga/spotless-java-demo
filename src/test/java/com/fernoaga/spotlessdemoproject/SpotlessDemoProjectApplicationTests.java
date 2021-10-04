07756bc40fe60c8697a23a2c576f9625f5c33f46
Vlad Fernoaga <fernoaga.vlad@gmail.com>
2021-10-04 17:07:48 +0300

    initial commit

---
+++ src/test/java/com/fernoaga/spotlessdemoproject/SpotlessDemoProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fernoaga.spotlessdemoproject;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessDemoProjectApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
