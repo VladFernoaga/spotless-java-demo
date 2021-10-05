70c89e6ed21cbd159edf66a8dde9fb170c35835a
Vlad Fernoaga <fernoaga.vlad@gmail.com>
2021-10-05 09:40:28 +0300

    integrate spotless to auto-format java code

--- src/main/java/com/fernoaga/spotlessdemoproject/SpotlessDemoProjectApplication.java
+++ src/main/java/com/fernoaga/spotlessdemoproject/SpotlessDemoProjectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpotlessDemoProjectApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpotlessDemoProjectApplication.class, args);
+         SpringApplication.run(SpotlessDemoProjectApplication.class, args);
   }
 
 }
